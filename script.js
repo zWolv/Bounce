5daa159568d436979480f7ba36fb6f2885503485
zWolv <samoht684@gmail.com>
2021-09-08 15:35:05 +0000

    bounce klaar

--- script.js
+++ script.js
@@ -9,11 +9,13 @@
 var circleY = [85,276,438,543,676];
 var circleX = [548,845,435,679,1000];
 
-var bouncePointX = [0,0,0,0,0];
-var bouncePointY = [0,0,0,0,0];
+var bouncePointXRight = [0,0,0,0,0];
+var bouncePointYUp = [0,0,0,0,0];
+var bouncePointXLeft = [0,0,0,0,0];
+var bouncePointYDown = [0,0,0,0,0];
 
-var circleWidth = [20,50,80,70,90];
-var circleHeight = [20,50,80,70,90];
+var circleWidth = [40,50,80,70,90];
+var circleHeight = [40,50,80,70,90];
 
 var speedX = [3,5,6,8,4];
 var speedY = [3,5,6,8,4];
@@ -28,9 +30,10 @@ function setup() {
 // bereken het punt waarop de zijkant van de cirkels zal zitten
 function bouncePoint() {
   for(x = 0;x < 5; x++){
-  bouncePointX[x] = 1280 - (circleWidth[x] / 2);
-  print(bouncePointX);
-  bouncePointY[x] = 720 - (circleHeight[x] / 2);
+  bouncePointXRight[x] = 1280 - (circleWidth[x] / 2);
+  bouncePointYUp[x] = 720 - (circleHeight[x] / 2);
+  bouncePointYDown[x] = 0 + (circleHeight[x] / 2);
+  bouncePointXLeft[x] = 0 + (circleWidth[x] / 2);
   }
 }
 
@@ -44,25 +47,16 @@ function updateCircle() {
   
 }
 
-// run alle functies
-function updateVariables() {
-  drawCircles();
-  bouncePoint();
-  bounceUpLeft();
-  //bounceDownRight(); // werkt niet
-  updateCircle();
-  print("kaas");
-  
-}
+
 
 // wanneer moeten de ballen omhoog of naar linksgaan
 function bounceUpLeft() {
   for(i = 0;i < 5;i++){
-    if(circleY[i] >= bouncePointY[i]) {
+    if(circleY[i] >= bouncePointYUp[i]) {
       speedY[i] = speedY[i] * -1;
     }
 
-    if(circleX[i] >= bouncePointX[i]) {
+    if(circleX[i] >= bouncePointXRight[i]) {
       speedX[i] = speedX[i] * -1;
     }
   }
@@ -71,26 +65,39 @@ function bounceUpLeft() {
 // wanneer moeten de ballen omlaag of naar rechts
 function bounceDownRight() {
   for(i = 0;i < 5;i++){
-    if(circleY[i] = 0) {
+    if(circleY[i] < bouncePointYDown[i]) {
       speedY[i] = speedY[i] * -1;
+      print("ja");
     }
 
-    if(circleX[i] = 0) {
+    if(circleX[i] < bouncePointXLeft[i]) {
       speedX[i] = speedX[i] * -1;
-      print('ja')
     }
   }
 }
 
 // teken de cirkels
 function drawCircles() {
-  fill(0,0,0);
+  fill(255,0,255);
   ellipse(circleX[0],circleY[0],circleWidth[0],circleHeight[0]);
+  fill(60,136,120);
   ellipse(circleX[1],circleY[1],circleWidth[1],circleHeight[1]);
+  fill(38,120,93);
   ellipse(circleX[2],circleY[2],circleWidth[2],circleHeight[2]);
+  fill(0,255,0);
   ellipse(circleX[3],circleY[3],circleWidth[3],circleHeight[3]);
+  fill(253,8,38);
   ellipse(circleX[4],circleY[4],circleWidth[4],circleHeight[4]);
 
+}
+
+// run alle functies
+function updateVariables() {
+  drawCircles();
+  bouncePoint();
+  bounceUpLeft();
+  bounceDownRight(); // werkt niet -- cirkels bouncen niet
+  updateCircle();
 }
 /**
  * draw
