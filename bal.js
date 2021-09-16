475d2dba8d790f83871231c1817d20350cef0ad1
zWolv <57898164+zWolv@users.noreply.github.com>
2021-09-16 16:59:29 +0200

    bal handmatig

--- bal.js
+++ bal.js
@@ -1,42 +1,33 @@
 class Bal {
-    x;
-    y;
-    speedX;
-    speedY;
-    kleur;
+  x;
+  y;
+  speedX;
+  speedY;
 
-    constructor(_x, _y) {
-        this.x = _x;
-        this.y = _y;
-        this.kleur,speedX = 2;
-        this.speedY = -3;
-        this.kleur = color(0,255,0);
+  constructor(_x, _y, _speedX, _speedY) {
+    this.x = _x;
+    this.y = _y;
+    this.speedX = _speedX;
+    this.speedY = _speedY;
+  }
+
+  // update de positie van de bal en klapt bij
+  // de kant de snelheid om
+  update() {
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+
+    if (this.x <=0 || this.x >= 1280) {
+      this.speedX = this.speedX * -1;
     }
-    
-    show() {
-        fill(this.kleur);
-        ellipse(this.x,this.y,69,69)
+    if (this.y <=0 || this.y >= 720) {
+      this.speedY = this.speedY * -1;
     }
+  }
 
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if(this.x <= 0) {
-            this.speedX = this.speedX * -1;
-        }
-
-        if(this.y <= 0) {
-            this.speedY = this.speedY * -1;
-        }
-
-        if(this.x >= 1211) {
-            this.speedX = this.speedX * -1;
-        }
-
-        if(this.y >= 651) {
-            this.speedY = this.speedY * -1;
-        }
-    }
+  // toont de bal
+  show() {
+    fill(255, 0, 0);
+    ellipse(this.x, this.y, 50, 50);
+  }
 }
-
-
