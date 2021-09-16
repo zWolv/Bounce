718325933682c49ecaa8804f8117a80662593000
zWolv <57898164+zWolv@users.noreply.github.com>
2021-09-16 16:59:02 +0200

    superbal handmatig

---
+++ superbal.js
@@ -0,0 +1,39 @@
+class Superbal {
+    x;
+    y;
+    speedX;
+    speedY;
+    kleurR;
+    kleurG;
+    kleurB;
+
+  constructor(_x, _y, _speedX, _speedY,_kleurR,_kleurG,kleurB) {
+    this.x = _x;
+    this.y = _y;
+    this.speedX = _speedX;
+    this.speedY = _speedY;
+    this.kleurB = _kleurB;
+    this.kleurG = _kleurG;
+    this.kleurR = _kleurR;
+  }
+
+  // update de positie van de bal en klapt bij
+  // de kant de snelheid om
+  update() {
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY;
+
+    if (this.x <=0 || this.x >= 1280) {
+      this.speedX = this.speedX * -1;
+    }
+    if (this.y <=0 || this.y >= 720) {
+      this.speedY = this.speedY * -1;
+    }
+  }
+
+  // toont de bal
+  show() {
+    fill(255, 0, 0);
+    ellipse(this.x, this.y, 50, 50);
+  }
+}
